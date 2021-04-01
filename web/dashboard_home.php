701c53fa5c306920b10b5a2f35fd638f9969dd27
pin jui <fee8ack21@gmail.com>
2021-04-01 15:06:18 +0800

    commit

--- web/dashboard_home.php
+++ web/dashboard_home.php
@@ -15,10 +15,12 @@ if (isset($_SESSION['admin_state'])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-    <title>後台</title>
+    <title>後台 - 首頁</title>
     <!--  -->
     <link rel="stylesheet" href="./css/all.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="shortcut icon" href="./images/favicon.ico" />
+    <link rel="bookmark" href="./images/favicon.ico" />
 </head>
 
 <body>
