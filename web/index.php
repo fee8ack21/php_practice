b857536e0d1030f926b0037bdc105ac62875d632
pin jui <fee8ack21@gmail.com>
2021-04-10 13:51:56 +0800

    add GA script

--- web/index.php
+++ web/index.php
@@ -50,6 +50,18 @@ if (isset($_SESSION['admin_state'])) {
     <!--  -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-H7S7GQX4P2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-H7S7GQX4P2');
+    </script>
     <script src="./js/all.js"></script>
 </body>
 
